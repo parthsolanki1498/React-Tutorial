86e74066339b34ad37adb76e56e6fffc9bff6d0f
Parth Solanki <parthsolanki1498@gmail.com>
2023-10-04 04:28:08 -0400

    bulma css added

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,15 +1,49 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+console.log(AlexaImage);
+console.log(SiriImage);
 
 function App() {
     return (
         <div>
-            <div>
-                Personal Digital Assistants
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Alexa"
+                                handle="@alexa99"
+                                image={AlexaImage}
+                                description="Alexa play React by Stephen Grider"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Cortana"
+                                handle="@cortana32"
+                                image={CortanaImage}
+                                description="Cortana was made by Microsoft, unlike ChatGPT"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Siri" 
+                                handle="@siri01" 
+                                image={SiriImage}
+                                description="Please continue with payment to know about your search query result"/>
+                        </div>
+                    </div>
+                </section>
             </div>
-
-            <ProfileCard/>
-            <ProfileCard/>
-            <ProfileCard/>
         </div>
     );
 }
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,5 +1,39 @@
-function ProfileCard() {
-    return <div> Profile Card </div>
+function ProfileCard({ title, handle, image, description}) {
+    // This can be used to avoid writing the "props." for multiple componenets over and over again
+    // same as using a variable 
+    // const title = props.title;
+    // const handle = props.handle;
+
+    // but considering we use the above mentioned approach, we're still writing .props for each prop
+    // for declaring variable which is again same as using it below in the div directly
+
+    // so we use Destructuring which goes something like which again is same as two lines above
+    // const { title, handle } = props;
+
+    // but here's the final hack that we can use to not even mention props and still use it
+    // it is in the argument itself along with destructing
+
+    // using this we can only take the properties we want instead of using the entire prop object
+
+    return (
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="pda logo" />
+                </figure>
+            </div>
+
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+                <div className="content">
+                    {description}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ProfileCard;
