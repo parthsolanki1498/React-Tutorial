8c2f5a64baef31689f63ded2fbb3f1aca81ffb65
Parth Solanki <parthsolanki1498@gmail.com>
2023-10-12 22:27:25 -0400

    final pics commit

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,5 +1,18 @@
-function ImageList() {
-    return <div>ImageList</div>
+import ImageShow from './ImageShow'
+import './ImageList.css'
+
+function ImageList( {images}) {
+
+    const renderedImages = images.map((image) => {
+        return (
+            // Always add the key to the top-most JSX element in the list
+            <div key={image.id}>
+                <ImageShow image={image} /> 
+            </div>
+        ) 
+    });
+
+    return <div className='image-list'>{renderedImages}</div>
 }
 
 export default ImageList;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,5 +1,36 @@
-function SearchBar() {
-    return <div>SearchBar</div>
+import { useState } from 'react';
+import './SearchBar.css'
+
+function SearchBar( {onSubmit} ) {
+
+    const [term, setTerm] = useState('');
+
+    const handleFormSubmit = (event) => {
+
+        // used to avoid the default behavior of form element
+        event.preventDefault();
+        // console.log('Need to pass data to parent');
+
+        // onSubmit(
+        //     // NEVER EVER EVER EVER DO THIS EVEN THO IT WORKS
+        //     document.querySelector('input').value
+        // );
+        onSubmit(term);
+    };
+
+    const handleChange = (event) => {
+        // console.log(event.target.value);
+        setTerm(event.target.value);
+    };
+
+    return (
+        <div className='search-bar'>
+            <form onSubmit={handleFormSubmit}>
+                <label>Enter Search Term</label>
+                <input value={term} onChange={handleChange} />
+            </form>
+        </div>
+    );
 }
 
 export default SearchBar;
