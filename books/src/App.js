fb825e9c3594ac81ab5f1747825a23fe956bbf40
Parth Solanki <parthsolanki1498@gmail.com>
2023-10-30 11:05:03 -0400

    final commit for books project

--- books/src/App.js
+++ books/src/App.js
@@ -12,7 +12,16 @@ function App() {
     // initially rendered or at a specific re-render
     useEffect(() => {
         fetchBooks();
-    },[]);
+    },[fetchBooks]);
+
+    // Never add fetchBooks() to the dependacy array to fix the ESLint warning ^^^
+    // We indeed need to add fetchBooks() but do additional things as well to ensure
+    // our web app doesn't end up making endless calls to our backend api
+
+    // useCallback hook for resuce - it tells React that function is not changing over time
+
+    // Also never create a function within useEffect arrow function which refers to a variable
+    // It will result in Stale Variable Bug
 
     // The arrow function within the useEffect is immediately called after first DOM update (render)
     // Second, Third and so on depends on the second argument of the useEffect function
--- books/src/context/books.js
+++ books/src/context/books.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useCallback } from 'react';
 import axios from 'axios';
 
 const BooksContext = createContext();
@@ -17,7 +17,7 @@ const BooksContext = createContext();
 function Provider({ children }) {
   const [books, setBooks] = useState([]);
 
-  const fetchBooks = async () => {
+  const fetchBooks = useCallback(async () => {
     const response = await axios.get('http://localhost:3001/books');
 
     setBooks(response.data);
@@ -49,7 +49,7 @@ function Provider({ children }) {
         // Tips : Avoid using push function when arrays and state function are used together or modifying an existing
         // array like array[0] = something or even avoid modifying property of an existing object.
 
-  };
+  },[]);
 
   const editBookById = async (id, newTitle) => {
     const response = await axios.put(`http://localhost:3001/books/${id}`, {
