2074fb52000d2ee4aefba79ba2c90bd4d2c56951
Parth Solanki <parthsolanki1498@gmail.com>
2025-01-07 23:45:00 -0500

    back to react tutorials because 2025 resolution

---
+++ comps/src/App.js
@@ -0,0 +1,25 @@
+import Button from "./Button";
+
+function App() {
+    return (
+        <div>
+            <div>
+                <Button>Click Me</Button>
+            </div>
+            <div>
+                <Button>Buy Now</Button>
+            </div>
+            <div>
+                <Button>See Deal</Button>
+            </div>
+            <div>
+                <Button>Hide Ads</Button>
+            </div>
+            <div>
+                <Button>Something cool</Button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
